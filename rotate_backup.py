48ef2d5cc529153821ad6d64c268a19ff22b2ad9
jshepherd <juhasz.janos.flutemaker@gmail.com>
2017-04-21 08:59:45 +0200

    Simplification, generalization

--- rotate_backup.py
+++ rotate_backup.py
@@ -7,36 +7,45 @@ import time
 class archive:
     def __init__(self, path):
         self.path = path
-        self.sec  = int(time.time() - os.path.getmtime(path))
-        self.min  = self.sec / 60
-        self.hour = self.min / 60
-        self.day  = self.hour / 24
-        self.week = self.day / 7
+        self.time = time.gmtime(os.path.getmtime(path))
+        self.year = time.strftime("%Y", self.time)
+        self.month = time.strftime("%Y%m", self.time)
+        self.week = time.strftime("%Y%W", self.time)
+        self.day = time.strftime("%Y%m%d", self.time)
+        self.hour = time.strftime("%Y%m%d%H", self.time)
+        self.min = time.strftime("%Y%m%d%H%M", self.time)
+        self.sec = time.strftime("%Y%m%d%H%M%S", self.time)
 
     def rm(self):
         print "rm %s" % self.path
         os.remove(self.path)
 
-def getBinHead(array, binNum, grouping_function):
-    bins = {}
+class binStoreNewest:
+    def __init__(self, binNum):
+        self.bins = {}
+        self.binNum = binNum
 
-    for a in array:
-        group = grouping_function(a)
-        if group in bins:  bins[group].append(a)
-        else:              bins[group] = [a]
+    def add(self, id, item):
+        if id in self.bins:  # there is a archive for this cluster id
+            storedItem = self.bins[id]
+            if storedItem.time < item.time:  # item is newer then the stored one
+                self.bins[id] = item
+        else:
+            self.bins[id] = item    # there wasn't archive for this cluster id still now
 
-    binkeys = bins.keys()
-    binkeys.sort() # sorting in youngest->oldest order
-    binkeys = binkeys[:binNum] # keep these bins
+        keys = self.bins.keys()
+        keys.sort()
+        for id in keys[:-self.binNum]:  # delete the archives out of scope, keep the last binNum ones
+            del self.bins[id]
 
-    keep = [bins[key] for key in binkeys]
+    def getPaths(self):
+        return [item.path for item in self.bins.values()]
 
-    def sec(item):
-        return item.sec
-
-    tops = [min(item, key=sec) for item in keep]
-    return set(tops)
-
+def getBinTops(sourceArray, binNum, clusterFunction):
+    binStore = binStoreNewest(binNum)
+    for item in sourceArray:
+        binStore.add(clusterFunction(item), item)
+    return binStore.getPaths()
 
 if __name__ == '__main__':
     if len(sys.argv) >= 2:
@@ -46,12 +55,13 @@ if __name__ == '__main__':
 
     archives = [archive(filename) for filename in files]
 
-    keep_daily = getBinHead(archives, 7, lambda x: x.day)
-    keep_weekly = getBinHead(archives, 4, lambda x: x.week)
-    keep_all = keep_daily.union(keep_weekly)
-    remove_files= set(archives).difference(keep_all)
+    daily = getBinTops(archives, 7, lambda item: item.day)
+    weekly = getBinTops(archives, 4, lambda item: item.week)
+    monthly = getBinTops(archives, 12, lambda item: item.month)
+    yearly = getBinTops(archives, 10, lambda item: item.year)
 
-    for item in remove_files:
-        item.rm()
+    keepPaths = daily + weekly + monthly + yearly
 
-
+    for item in archives:
+        if item.path not in keepPaths:
+            item.rm()
--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-for i in `seq 100`; do touch -d "-$i days" "data/backup_$i.tar.gz"; done
+for i in `seq 2000`; do touch -d "-$i days" "data/backup_$i.tar.gz"; done
